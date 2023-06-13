56ac9cf5bea07618b64e9fa2698aa24276ef0463
Adriano G <adrianogomesfilho98@gmail.com>
2023-06-13 20:39:51 -0300

    grfdgb

--- primeiroprograma.js
+++ primeiroprograma.js
@@ -1,1 +1,11 @@
 console.log ("My first code");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
