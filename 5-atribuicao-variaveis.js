1840edb0a501164cafc6733d64bc7c60a758b367
Adriano G <adrianogomesfilho98@gmail.com>
2023-06-14 20:31:29 -0300

    .

---
+++ 5-atribuicao-variaveis.js
@@ -0,0 +1,19 @@
+console.log("Conversão de tipos");
+let nome = "Ricardo";
+const sobrenome = "Bugan";
+
+// console.log (nome + " " + sobrenome);
+console.log (nome, sobrenome); //fica igual
+console.log (`Meu nome é ${nome} ${sobrenome}`);
+
+let contador = 0
+contador = contador + 1
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; // declarando variavel
+idade = 26 //atribuindo valor
+idade = idade + 1
+
+console.log(idade);
