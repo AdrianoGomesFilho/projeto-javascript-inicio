5b3cdacbb81dada797cdf546f2b9969a1fe1546a
Adriano G <adrianogomesfilho98@gmail.com>
2023-06-20 20:30:45 -0300

    .

--- 8-loops.js
+++ 8-loops.js
@@ -6,10 +6,10 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`,
 );
 
-const idadeComprador = 17;
+const idadeComprador = 19;
 const estaAcompanhada = false;
 let temPassagemComprada = false;
-const destino = `Rio de Janeiro`;
+const destino = `Salvd`;
 
 
 console.log(`\n Destinos possíveis:`);
@@ -18,13 +18,20 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador = 0;
-while(contador<3) {
+let destinoExiste = false;
 
-    if (listaDeDestinos[contador] == destino) {
-        console.log(`Destino existe`);
-    } else {
-        console.log(`Destino não existe`);
-    }
+
+console.log(`Destino existe: `, destinoExiste);
+
+if (podeComprar && destinoExiste) {
+    console.log(`Boa viagem!`)
+} else {
+    console.log(`Desculpe, houve um erro!`)
+}
+
+for(let i=0;i<3;i++) {
     
-    contador += 1; 
+    if (listaDeDestinos[i] == destino) {
+        destinoExiste = true
+    }
 }
